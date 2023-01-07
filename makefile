06405490481a7e7fee14d2ffdbd9b2fc002f64bc
zikrullah79 <zikrullahaliwaindah@gmail.com>
2023-01-07 17:00:11 +0700

    initial landing page

---
+++ makefile
@@ -0,0 +1,2 @@
+run:
+	npx tailwindcss -i ./css/main.css -o ./dist/output.css --watch
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      
+      spacing: {
+        'standard': '100px',
+        'mobile': '20px',
+      }
     },
+    dropShadow: {
+      'basic-blue': '0 35px 35px rgba(91, 192, 248, 0.45)',
+    }
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["bumblebee"],
+  },
 }
