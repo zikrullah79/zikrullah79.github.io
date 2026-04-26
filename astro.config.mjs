978128533742da5bc1d955188c52c0c1401a9378
zikrullah79 <zikrullahaliwaindah@gmail.com>
2026-04-26 16:43:57 +0700

    chore: add github actions deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zikrullah79.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
